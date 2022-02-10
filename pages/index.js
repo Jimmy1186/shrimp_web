29a5da36be585f8083aaf89d1c2b06de799c1c93
Jimmy1186 <sususu089@gmail.com>
2022-02-10 16:50:20 +0800

    fix error

--- pages/index.js
+++ pages/index.js
@@ -11,38 +11,63 @@ export default function Home() {
   const [payload9, setPayload9] = useState([]);
   const [payload10, setPayload10] = useState([]);
 
-
-
   useEffect(() => {
-     const fetchData = async () => {
-        const result8 = await axios("http://localhost:3000/api/pond8Data");
-        const result9 = await axios("http://localhost:3000/api/pond9Data");
-        const result10 = await axios("http://localhost:3000/api/pond10Data");
+    const fetchData = async () => {
+      const result8 = await axios("http://localhost:3000/api/pond8Data").catch(
+        function (error) {
+          console.log(error);
+        }
+      );
+      const result9 = await axios("http://localhost:3000/api/pond9Data").catch(
+        function (error) {
+          console.log(error);
+        }
+      );
+      const result10 = await axios(
+        "http://localhost:3000/api/pond10Data"
+      ).catch(function (error) {
+        console.log(error);
+      });
+      try {
         setPayload8(result8.data);
         setPayload9(result9.data);
         setPayload10(result10.data);
-      };
-fetchData();
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-    setInterval(async() => {
-     
+    fetchData();
+
+    setInterval(async () => {
       fetchData();
-      
     }, 5000);
-   return ()=>{
-     clearInterval()
-   }
+    return () => {
+      clearInterval();
+    };
   }, []);
   return (
     <>
       <Head>
         <title>魚池參數</title>
       </Head>
-    
+
       <div className="wrapper">
-      {payload8.length==0 ? (  <Loading pondTitle={8} />):(<Ponds pondTitle={8} data={payload8}/>)}
-        {payload9.length==0 ? (  <Loading pondTitle={9} />):(<Ponds pondTitle={9} data={payload9}/>)}
-        {payload10.length==0 ? (  <Loading pondTitle={10} />):(<Ponds pondTitle={10} data={payload10}/>)}
+        {payload8.length == 0 ? (
+          <Loading pondTitle={8} />
+        ) : (
+          <Ponds pondTitle={8} data={payload8} />
+        )}
+        {payload9.length == 0 ? (
+          <Loading pondTitle={9} />
+        ) : (
+          <Ponds pondTitle={9} data={payload9} />
+        )}
+        {payload10.length == 0 ? (
+          <Loading pondTitle={10} />
+        ) : (
+          <Ponds pondTitle={10} data={payload10} />
+        )}
       </div>
     </>
   );

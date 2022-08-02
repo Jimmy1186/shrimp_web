ab6998c16689bd5b5a53d939fdc5eecbd5fefc1b
Jimmy1186 <sususu089@gmail.com>
2022-08-02 21:00:44 +0800

    add pond6 and 7

---
+++ pages/api/pond7Data.js
@@ -0,0 +1,9 @@
+const { POND_7_COLLECTION } = require("../../models/pond");
+import dbConnect from "../../utils/dbConnection";
+dbConnect();
+let payload;
+export default async function handler(req, res) {
+  const payload = await POND_7_COLLECTION.find().sort({'_id': -1}).limit(1);
+  res.json(payload);
+
+}
--- pages/index.js
+++ pages/index.js
@@ -5,18 +5,31 @@ import axios from "axios";
 import Loading from "../components/Loading";
 
 const DNS = process.env.DNS;
+const POND_6_API = process.env.POND_6_API;
+const POND_7_API = process.env.POND_7_API;
 const POND_8_API = process.env.POND_8_API;
 const POND_9_API = process.env.POND_9_API;
 const POND_10_API = process.env.POND_10_API;
 
-
 export default function Home() {
+  const [payload6, setPayload6] = useState([]);
+  const [payload7, setPayload7] = useState([]);
   const [payload8, setPayload8] = useState([]);
   const [payload9, setPayload9] = useState([]);
   const [payload10, setPayload10] = useState([]);
-//
+  //
   useEffect(() => {
     const fetchData = async () => {
+      const result6 = await axios(`${DNS}+${POND_6_API}`).catch(function (
+        error
+      ) {
+        console.log(error);
+      });
+      const result7 = await axios(`${DNS}+${POND_7_API}`).catch(function (
+        error
+      ) {
+        console.log(error);
+      });
       const result8 = await axios(`${DNS}+${POND_8_API}`).catch(function (
         error
       ) {
@@ -33,6 +46,8 @@ export default function Home() {
         console.log(error);
       });
       try {
+        setPayload6(result6.data);
+        setPayload7(result7.data);
         setPayload8(result8.data);
         setPayload9(result9.data);
         setPayload10(result10.data);
@@ -57,6 +72,16 @@ export default function Home() {
       </Head>
 
       <div className="wrapper">
+        {payload6.length == 0 ? (
+          <Loading pondTitle={6} />
+        ) : (
+          <Ponds pondTitle={6} data={payload6} />
+        )}
+        {payload7.length == 0 ? (
+          <Loading pondTitle={7} />
+        ) : (
+          <Ponds pondTitle={7} data={payload7} />
+        )}
         {payload8.length == 0 ? (
           <Loading pondTitle={8} />
         ) : (

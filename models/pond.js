d224b8e4515b9cbf7eb4617be915164e4eb22123
Jimmy1186 <sususu089@gmail.com>
2022-08-02 21:00:44 +0800

    add pond6 and 7

--- models/pond.js
+++ models/pond.js
@@ -14,12 +14,17 @@ const pondSchema = new Schema({
   TIME:String
 })
 
+const POND_6_COLLECTION = mongoose.models.pond_6 || mongoose.model('pond_6',pondSchema );
+const POND_7_COLLECTION =  mongoose.models.pond_7 || mongoose.model('pond_7',pondSchema );
+
 const POND_8_COLLECTION = mongoose.models.pond_8 || mongoose.model('pond_8',pondSchema );
 const POND_9_COLLECTION =  mongoose.models.pond_9 || mongoose.model('pond_9',pondSchema );
 const POND_10_COLLECTION =    mongoose.models.pond_10 || mongoose.model('pond_10',pondSchema );
 
 
 module.exports = {
+  POND_6_COLLECTION,
+  POND_7_COLLECTION,
   POND_8_COLLECTION,
   POND_9_COLLECTION,
   POND_10_COLLECTION,
